a82bf7525518dda3ac8ee9e8ec53cdbaa8b13cab
Lucas Javier Juarez <lucasjavierjuarez@hotmail.com>
2016-09-26 14:14:51 -0300

    Adding constants

---
+++ src/BackofficeServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace LucasRuroken\Backoffice;
+
+use Illuminate\Support\ServiceProvider;
+use LucasRuroken\Backoffice\Constants\ViewConstants;
+
+class BackofficeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/Views/', ViewConstants::BACKOFFICE_INDEX);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/Constants/ViewConstants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LucasRuroken\Backoffice\Constants;
+
+class ViewConstants
+{
+    const BACKOFFICE_INDEX = 'backoffice_index';
+
+}
