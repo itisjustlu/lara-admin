cd3bd01a9553e455759efebdb9fec24cdfdc977e
Lucas Javier Juarez <lucasjavierjuarez@hotmail.com>
2016-09-26 21:30:12 -0300

    A few changes :yellow_heart:

--- src/Config/admin.php
+++ src/Config/admin.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
+
+    /**
+     * Set the current template of this admin application
+     * You can set 'gentelella', 'adminlte'
+     */
+    'template' => 'gentelella',
+
     'assets_path' => public_path('assets')
 ];
